b4c955101122ebbc68cd4af0edd618835f0d1258
mehrdad.kazazi70 <mehrdad.kazazi70@gmail.com>
2020-12-24 18:01:20 +0330

    Create project

---
+++ src/main/java/com/sharifuc/sharifuc/SharifucApplication.java
@@ -0,0 +1,13 @@
+package com.sharifuc.sharifuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharifucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharifucApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharifuc/sharifuc/SharifucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharifuc.sharifuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharifucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
